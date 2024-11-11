6ec1f102f6b2bb98625638ac000b2bd5ac4b0511
zgr <2539336179@qq.com>
2024-11-12 04:18:14 +0800

    Put the running process of HITS into the method

--- src/main/java/zju/cst/aces/runner/MethodRunner.java
+++ src/main/java/zju/cst/aces/runner/MethodRunner.java
@@ -1,13 +1,11 @@
 package zju.cst.aces.runner;
 
-import zju.cst.aces.api.phase.Phase;
+import zju.cst.aces.api.phase.PhaseImpl;
 import zju.cst.aces.api.config.Config;
-import zju.cst.aces.api.impl.ChatGenerator;
 import zju.cst.aces.api.impl.PromptConstructorImpl;
 import zju.cst.aces.api.impl.obfuscator.Obfuscator;
-import zju.cst.aces.api.phase.Phase_HITS;
+import zju.cst.aces.api.phase.solution.HITS;
 import zju.cst.aces.dto.*;
-import zju.cst.aces.prompt.PromptGenerator;
 import zju.cst.aces.util.JsonResponseProcessor;
 
 import java.io.IOException;
@@ -60,10 +58,64 @@ public class MethodRunner extends ClassRunner {
         }
     }
 
+    public boolean startHITSRounds(final int num, PhaseImpl phase, PromptConstructorImpl pc) {
+        HITS phase_hits = (HITS) phase;
+        PromptInfo promptInfo = pc.getPromptInfo();
+        if (config.isEnableObfuscate()) {
+            Obfuscator obfuscator = new Obfuscator(config);
+            PromptInfo obfuscatedPromptInfo = new PromptInfo(promptInfo);
+            obfuscator.obfuscatePromptInfo(obfuscatedPromptInfo);
+
+            phase_hits.generateMethodSlice(pc);
+        } else {
+            phase_hits.generateMethodSlice(pc);
+        }
+        JsonResponseProcessor.JsonData methodSliceInfo = JsonResponseProcessor.readJsonFromFile(promptInfo.getMethodSlicePath().resolve("slice.json"));
+        if (methodSliceInfo != null) {
+            // Accessing the steps
+            boolean hasErrors;
+            for (int i = 0; i < methodSliceInfo.getSteps().size(); i++) { //这里还要改
+                // Test Generation Phase
+                hasErrors = false;
+                if (methodSliceInfo.getSteps().get(i) == null) continue;
+                promptInfo.setSliceNum(i);
+                promptInfo.setSliceStep(methodSliceInfo.getSteps().get(i)); // todo 存储切片信息到
+                phase_hits.generateSliceTest(pc); //todo 改成新的hits对切片生成单元测试方法
+                // Validation
+                if (phase_hits.validateTest(pc)) {
+                    exportRecord(pc.getPromptInfo(), classInfo, num);
+                    return true;
+                } else {
+                    hasErrors = true;
+                }
+                if (hasErrors) {
+                    // Validation and Repair Phase
+                    for (int rounds = 0; rounds < config.getMaxRounds(); rounds++) {
+
+                        promptInfo.setRound(rounds);
+
+                        // Repair
+                        phase_hits.repairTest(pc);
+
+                        // Validation and process
+                        if (phase_hits.validateTest(pc)) { // if passed validation
+                            exportRecord(pc.getPromptInfo(), classInfo, num);
+                            return true; // successfully
+                        }
+
+                    }
+                }
+
+                exportSliceRecord(pc.getPromptInfo(), classInfo, num, i); //todo 检测是否顺利生成信息
+            }
+        }
+        return false;
+    }
+
     public boolean startRounds(final int num) {
 
         config.setPhaseType("HITS");
-        Phase phase = Phase.createPhase(config);
+        PhaseImpl phase = PhaseImpl.createPhase(config);
 
         // Prompt Construction Phase
         PromptConstructorImpl pc = phase.generatePrompt(classInfo, methodInfo,num);
@@ -71,54 +123,7 @@ public class MethodRunner extends ClassRunner {
         promptInfo.setRound(0);
 
         if(config.getPhaseType() == "HITS") {
-            Phase_HITS phase_hits = (Phase_HITS) phase;
-            if (config.isEnableObfuscate()) {
-                Obfuscator obfuscator = new Obfuscator(config);
-                PromptInfo obfuscatedPromptInfo = new PromptInfo(promptInfo);
-                obfuscator.obfuscatePromptInfo(obfuscatedPromptInfo);
-
-                phase_hits.generateMethodSlice(pc);
-            } else {
-                phase_hits.generateMethodSlice(pc);
-            }
-            JsonResponseProcessor.JsonData methodSliceInfo = JsonResponseProcessor.readJsonFromFile(promptInfo.getMethodSlicePath().resolve("slice.json"));
-            if (methodSliceInfo != null) {
-                // Accessing the steps
-                boolean hasErrors;
-                for (int i = 0; i < methodSliceInfo.getSteps().size(); i++) { //这里还要改
-                    // Test Generation Phase
-                    hasErrors = false;
-                    if (methodSliceInfo.getSteps().get(i) == null) continue;
-                    promptInfo.setSliceNum(i);
-                    promptInfo.setSliceStep(methodSliceInfo.getSteps().get(i)); // todo 存储切片信息到
-                    phase_hits.generateSliceTest(pc); //todo 改成新的hits对切片生成单元测试方法
-                    // Validation
-                    if (phase_hits.validateTest(pc)) {
-                        exportRecord(pc.getPromptInfo(), classInfo, num);
-                    } else {
-                        hasErrors = true;
-                    }
-                    if (hasErrors) {
-                        // Validation and Repair Phase
-                        for (int rounds = 0; rounds < config.getMaxRounds(); rounds++) {
-
-                            promptInfo.setRound(rounds);
-
-                            // Repair
-                            phase_hits.repairTest(pc);
-
-                            // Validation and process
-                            if (phase_hits.validateTest(pc)) { // if passed validation
-                                exportRecord(pc.getPromptInfo(), classInfo, num);
-                                break; // successfully
-                            }
-
-                        }
-                    }
-
-                    exportSliceRecord(pc.getPromptInfo(), classInfo, num, i); //todo 检测是否顺利生成信息
-                }
-            }
+            startHITSRounds(num, phase, pc);
         }
 
 
